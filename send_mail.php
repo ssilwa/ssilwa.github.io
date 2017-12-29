4f49b7aa45846ef3edc4ee4b4d234abc8af750ab
ssilwa <silwal@mit.edu>
2017-12-28 19:42:18 -0500

    updated php

--- send_mail.php
+++ send_mail.php
@@ -10,8 +10,8 @@ This bit sets the URLs of the supporting pages.
 If you change the names of any of the pages, you will need to change the values here.
 */
 $feedback_page = "contact.html";
-$error_page = "error_message.html";
-$thankyou_page = "thank_you.html";
+$error_page = "index.html";
+$thankyou_page = "index.html";
 
 /*
 This next bit loads the form field data into variables.
